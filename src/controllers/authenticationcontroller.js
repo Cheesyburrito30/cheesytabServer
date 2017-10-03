5ef7aa7fa3f60e05a07ee2f618d625054038f86d
Vernon Perry <pvernon30@gmail.com>
2017-10-03 15:42:25 -0400

    test

--- src/controllers/authenticationcontroller.js
+++ src/controllers/authenticationcontroller.js
@@ -1,6 +1,5 @@
 const {User} = require ('../models')
 const jwt = require('jsonwebtoken')
-const config = require('../config/config')
 function jwtSignUser (user) {
 	const ONE_WEEK = 60* 60 * 24 * 7
 	return jwt.sign(user, config.authentication.jwtSecret, {
